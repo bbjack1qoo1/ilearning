87784e39b31782e2288f9b346cb30cf5e3a5c22e
awei1995 <awei1995@users.noreply.github.com>
2018-10-22 21:01:20 +0800

    nothing
    nothing

---
+++ templates_c/67982f17b41457e2a1d4a160c771f60f286031b1_0.file.teach.html.php
@@ -0,0 +1,35 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-10-20 16:14:35
+  from 'C:\Users\a0979\Documents\GitHub\ilearning\templates\teach.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5bcb384b56b100_05733497',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '67982f17b41457e2a1d4a160c771f60f286031b1' => 
+    array (
+      0 => 'C:\\Users\\a0979\\Documents\\GitHub\\ilearning\\templates\\teach.html',
+      1 => 1539764662,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bcb384b56b100_05733497 (Smarty_Internal_Template $_smarty_tpl) {
+?><html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>教學內容</title>
+</head>
+<body>
+    <p>教學內容頁面</p>
+</body>
+</html><?php }
+}
